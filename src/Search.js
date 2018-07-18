fd66c3c64c6315cfe15b12ca1a801640986012d8
marwaaboshall <marwa.aboshall@gmail.com>
2018-07-19 00:13:59 +0200

    add search to the app

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
-
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+import Book from './Book';
 
 class Search extends Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim()})
+    }
     render() {
-        //return()
+        let showingBooks;
+        if(this.state.query) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i');
+            showingBooks = this.props.books.filter((book) => match.test(book.title))
+        } else {
+            showingBooks = this.props.books;
+        }
+        showingBooks.sort(sortBy('name'));
+        return(
+            <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" 
+                       placeholder="Search by title or author"
+                       value={this.state.query} 
+                       onChange={(event) => this.updateQuery(event.target.value)}/>
+                 {/* {JSON.stringify(this.state)} */}
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+              {showingBooks.map(book => <Book book={book} 
+                                              key={book.id} 
+                                              updateBookShelf={this.props.changeBookShelf}/>)}
+              </ol>
+            </div>
+          </div>
+        )
     }
 }
 
